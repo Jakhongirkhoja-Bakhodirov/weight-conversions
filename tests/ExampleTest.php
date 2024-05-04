a24e8efb77564a6f98722f85168301a5dfc0efce
Bakhodirov-Jakhongir <Bakhodirov-Jakhongir@users.noreply.github.com>
2024-05-04 17:25:29 +0000

    Fix styling

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -3,6 +3,6 @@
 use Bakhodirov\WeightConversions\Weight;
 
 it('it converts to kilograms to pounds', function () {
-    $pounds =  Weight::kilograms(100)->toPounds();
+    $pounds = Weight::kilograms(100)->toPounds();
     expect($pounds)->toEqual(220.46);
 });
