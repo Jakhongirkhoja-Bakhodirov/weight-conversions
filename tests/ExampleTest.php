b78f42e58cec875c8a280e82164dbd4a9efdede1
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2024-05-04 22:25:07 +0500

    create Weight class for convertion and write unit tests on pest

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,8 @@
 <?php
 
-it('can test', function () {
-    expect(true)->toBeTrue();
+use Bakhodirov\WeightConversions\Weight;
+
+it('it converts to kilograms to pounds', function () {
+    $pounds =  Weight::kilograms(100)->toPounds();
+    expect($pounds)->toEqual(220.46);
 });
